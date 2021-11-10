b4428247a4784b3ac60d9afcfd025a4faf961cd4
Bojan Gavrovski <bojan@beyondbasics.co>
2021-11-10 20:26:57 +0100

    file download

--- c071/handlers/storage.js
+++ c071/handlers/storage.js
@@ -1,8 +1,10 @@
+const fs = require('fs');
+const strings = require('../pkg/strings');
+
 const MAX_FILESIZE = 1024 * 1024 * 1; // 1 MB
 const ALLOWED_FILETYPES = ['image/jpg', 'image/jpeg', 'image/pjpg', 'image/png', 'image/gif'];
 
 const upload = async (req, res) => {
-    console.log(req.files);
 
     if(req.files.document.size > MAX_FILESIZE) {
         return res.status(400).send('File exceeds max file size');
@@ -12,18 +14,34 @@ const upload = async (req, res) => {
         return res.status(400).send('Filetype not allowed');
     }
 
-    let filePath = `${__dirname}/../uploads/${req.files.document.name}`;
+    let userDir = `user_${req.user.uid}`;
+    let userDirPath = `${__dirname}/../uploads/${userDir}`;
+
+    if(!fs.existsSync(userDirPath)) {
+        fs.mkdirSync(userDirPath);
+    }
+
+    let fileID = strings.makeID(6);
+    let fileName = `${fileID}_${req.files.document.name}`;
+    let filePath = `${userDirPath}/${fileName}`;
     req.files.document.mv(filePath, err => {
         if(err) {
             console.log(err);
             return res.status(500).send('Internal server error');
         }
-        res.status(200).send('ok');
+        res.status(200).send({filename: fileName});
     });
 };
 
 const download = async (req, res) => {
-    res.status(200).send('ok');
+    let userDir = `user_${req.user.uid}`;
+    let userDirPath = `${__dirname}/../uploads/${userDir}`;
+    let filePath = `${userDirPath}/${req.params.filename}`;
+
+    if(!fs.existsSync(filePath)) {
+        return res.status(404).send('Not Found');
+    }
+    res.download(filePath);
 };
 
 module.exports = {
